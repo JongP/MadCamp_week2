1f85dc1ba74454c1e83c3f6635836e7d65317f08
JYBESiU <jychoi.besiu@gmail.com>
2021-07-01 11:16:47 +0900

    'ignore'

--- app/src/main/java/com/example/madcamp_week1/Fragment2.java
+++ app/src/main/java/com/example/madcamp_week1/Fragment2.java
@@ -60,7 +60,7 @@ public class Fragment2 extends Fragment {
         }
 
         ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             list.add(String.format("Test %d", i));
         }
 
