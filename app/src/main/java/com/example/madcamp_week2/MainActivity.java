9ef4dcaeaadda9cf16e81f63103ed92f0218548f
JongP <jong.p@kaist.ac.kr>
2021-07-09 23:53:10 +0900

    Frag3/login done. db+login should be done.

--- app/src/main/java/com/example/madcamp_week2/MainActivity.java
+++ app/src/main/java/com/example/madcamp_week2/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,13 +24,19 @@ import java.security.NoSuchAlgorithmException;
 public class MainActivity extends AppCompatActivity {
     CustomViewPager viewPager;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         setTheme(R.style.NoBarTheme);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         viewPager = findViewById(R.id.view_pager);
@@ -36,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
 
-        getHashKey();
+
     }
 
 
@@ -47,25 +56,4 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void getHashKey(){
-
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
-            }
-        }
-    }
 }
